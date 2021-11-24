6d80be99d5f593bf3dea1c6dd0727d771b898cda
hammelikram <ikramhammel44@gmail.com>
2021-11-24 21:15:06 +0100

    Display a list of images using cards

--- app/src/main/java/com/ikram/affirmation/adapter/ListItem.kt
+++ app/src/main/java/com/ikram/affirmation/adapter/ListItem.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.model.Affirmation
 import com.ikram.affirmation.R
-import com.ikram.affirmation.model.Affirmation
 
 /**
  * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
